c6a47240dd1de69bc9c07105a5bda83539d3a4ec
Obeaune <linecora86@gmail.com>
2021-11-08 05:17:32 -0300

    Condições If e Else

---
+++ fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-01-primeiros-passos/scripts.js
@@ -0,0 +1,10 @@
+const notaDesafio = 90;
+if (notaDesafio >= 80) {
+  console.log("Parabéns, você foi aprovado");
+}
+else if (notaDesafio < 80 && notaDesafio >= 60) {
+  console.log("Você está na nossa lista de espera");
+}
+else {
+  console.log("Você foi reprovada(o)");
+}
