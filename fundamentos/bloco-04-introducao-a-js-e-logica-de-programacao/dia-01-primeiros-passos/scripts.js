b72b72cb17c00eec41365382ccc7a2790a49eda2
Golb <golb@ana-pc.localdomain>
2021-11-10 23:02:11 -0300

    Exercicio Fixacao 5

--- fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-01-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-01-primeiros-passos/scripts.js
@@ -1,9 +1,18 @@
-// Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados. 
-const a = 5;
-if (a > 0) {
-  console.log("Positive");
-} else if (a < 0){
-  console.log("Negative");
+// Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro. 
+// Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus. 
+// Um ângulo será considerado inválido se não tiver um valor positivo. 
+
+let angA = 70;
+let angB = 40;
+let angC = 70;
+let somaDosAngulos = angA + angB + angC;
+
+if (angA > 0 && angB > 0 && angC > 0) {
+  if (somaDosAngulos === 180) {
+    console.log("True.");
+  } else {
+    console.log("False.");
+  }
 } else {
-  console.log("Zero");
+  console.log("Erro: ângulo inválido.");
 }
