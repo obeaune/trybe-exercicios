b7876e261e0e0f81af81a0d72bab2bdd00aa4f6d
Golb <golb@ana-pc.localdomain>
2021-11-11 02:10:44 -0300

    Exercicio Fixacao 2

--- fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/scripts.js
@@ -1,6 +1,8 @@
-// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ; 
+// Para o segundo exercício, some todos os valores contidos no array e imprima o resultado; 
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for (let numero of numbers) {
-  console.log(numero);
+let soma = 0;
+for (let i = 0; i < numbers.length; i += 1) {
+  soma += numbers[i];
 }
+console.log(soma);
