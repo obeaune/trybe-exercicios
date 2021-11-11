0613eb2af4d48296f3835bc1f81217e0e8c59bd7
Golb <golb@ana-pc.localdomain>
2021-11-11 01:29:22 -0300

    Exercicio Fixacao 11

--- fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-01-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-01-primeiros-passos/scripts.js
@@ -1,18 +1,26 @@
-// Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos. 
-// Atente que, sobre o custo do produto, incide um imposto de 20%. 
-// Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero. 
-// O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
-//     - valorCustoTotal = valorCusto + impostoSobreOCusto
-//     - lucro = valorVenda - valorCustoTotal (lucro de um produto)
+const salarioBruto = 3000;
+let salarioBase;
+let aliquotaIR;
 
-const valorCusto = 100;
-const valorVenda = 150;
-
-if (valorCusto >= 0 && valorVenda >= 0) {
-  const impostoSobreOCusto = (20 / valorCusto) * 100;
-  const valorCustoTotal = valorCusto + impostoSobreOCusto;
-  const lucro = valorVenda - valorCustoTotal;
-  console.log("O lucro total foi: " + (1000 * lucro) + ".");
+if (salarioBruto <= 1556.94) {
+  salarioBase = salarioBruto - (salarioBruto * 0.08);
+} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+  salarioBase = salarioBruto - (salarioBruto * 0.09);
+} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+  salarioBase = salarioBruto - (salarioBruto * 0.11);
 } else {
-  console.log("Erro, os valores não podem ser menores que zero.");
+  salarioBase = salarioBruto - 570.88;
 }
+
+if (salarioBase <= 1903.98) {
+  aliquotaIR = 0;
+} else if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
+  aliquotaIR = (salarioBase * 0.075) - 142.80;
+} else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
+  aliquotaIR = (salarioBase * 0.15) - 354.80;
+} else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+  aliquotaIR = (salarioBase * 0.225) - 636.13;
+} else {
+  aliquotaIR = (salarioBase * 0.275) - 869.36;
+}
+console.log("Seu salário líquido é de R$ " + (salarioBase - aliquotaIR) + ".");
