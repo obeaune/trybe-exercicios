1f702c1f192c6b299827984e1398cc63fa505088
Golb <golb@ana-pc.localdomain>
2021-11-11 02:12:56 -0300

    Exercicio Fixacao 3

--- fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/scripts.js
@@ -1,8 +1,8 @@
-// Para o segundo exercício, some todos os valores contidos no array e imprima o resultado; 
+// Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array; 
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 for (let i = 0; i < numbers.length; i += 1) {
   soma += numbers[i];
 }
-console.log(soma);
+console.log(soma / numbers.length);
