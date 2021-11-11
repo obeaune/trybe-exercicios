8324a08d7a0c63a1d9814b6d807a62ecea0d18da
Golb <golb@ana-pc.localdomain>
2021-11-10 23:48:26 -0300

    Exercicio Fixacao 10

--- fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-01-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-01-primeiros-passos/scripts.js
@@ -1,10 +1,18 @@
-// Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false . 
+// Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos. 
+// Atente que, sobre o custo do produto, incide um imposto de 20%. 
+// Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero. 
+// O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
+//     - valorCustoTotal = valorCusto + impostoSobreOCusto
+//     - lucro = valorVenda - valorCustoTotal (lucro de um produto)
 
-const a = 5;
-const b = 18;
-const c = 125;
-if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
-  console.log("True");
+const valorCusto = 100;
+const valorVenda = 150;
+
+if (valorCusto >= 0 && valorVenda >= 0) {
+  const impostoSobreOCusto = (20 / valorCusto) * 100;
+  const valorCustoTotal = valorCusto + impostoSobreOCusto;
+  const lucro = valorVenda - valorCustoTotal;
+  console.log("O lucro total foi: " + (1000 * lucro) + ".");
 } else {
-  console.log("False");
+  console.log("Erro, os valores não podem ser menores que zero.");
 }
