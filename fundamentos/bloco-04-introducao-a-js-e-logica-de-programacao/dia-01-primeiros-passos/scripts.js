d276aa738485b9b17012c181bbab69ed32b0a879
Golb <golb@ana-pc.localdomain>
2021-11-10 22:50:37 -0300

    Exercicio Fixacao 4

--- fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-01-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-01-primeiros-passos/scripts.js
@@ -1,11 +1,9 @@
 // Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados. 
 const a = 5;
-const b = 20;
-const c = 13;
-if (a > b && a > c) {
-  console.log("O maior número é o da const A: " + a);
-} else if (b > a && b > c){
-  console.log("O maior número é o da const B: " + b);
+if (a > 0) {
+  console.log("Positive");
+} else if (a < 0){
+  console.log("Negative");
 } else {
-  console.log("O maior número é o da const C: " + c);
+  console.log("Zero");
 }
