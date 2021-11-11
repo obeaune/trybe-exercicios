bfc429bcf06f4bee62df0c6e40782c53aea91031
Golb <golb@ana-pc.localdomain>
2021-11-10 23:30:11 -0300

    Exercicio Fixacao 9

--- fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-01-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-js-e-logica-de-programacao/dia-01-primeiros-passos/scripts.js
@@ -1,9 +1,9 @@
-// Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false . 
+// Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false . 
 
-const a = 3;
+const a = 5;
 const b = 18;
 const c = 125;
-if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
   console.log("True");
 } else {
   console.log("False");
